2fa47c49c613d159f85aa1c58bf3d8da1c24f2be
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2021-10-30 19:38:59 +0300

    Set nixpkgs to track nixos-unstable.

--- flake.nix
+++ flake.nix
@@ -1,13 +1,27 @@
 {
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./machines/nixos-desktop/configuration.nix
-        ./common
-        ./optional/xserver
-        ./optional/texlive
-      ];
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
+
+  outputs = { self, nixpkgs }:
+    let inherit-nixpkgs = {
+      nix.registry = {
+        nixpkgs.flake = nixpkgs;
+        p.flake = nixpkgs;
+      };
+    }; in
+    {
+      nixosConfigurations = {
+        nixos-desktop = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./machines/nixos-desktop/configuration.nix
+            ./common
+            ./optional/xserver
+            ./optional/texlive
+            inherit-nixpkgs
+          ];
+        };
+      };
+    };
 }
