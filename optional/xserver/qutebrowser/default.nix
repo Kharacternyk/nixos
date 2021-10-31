7e9c23548f6f72308179c57355561a15d4930e1a
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2021-10-31 23:25:20 +0200

    Apply fixes suggested by statix.

--- optional/xserver/qutebrowser/default.nix
+++ optional/xserver/qutebrowser/default.nix
@@ -1,26 +1,28 @@
 { pkgs, ... }:
-let qutebrowserWithQutelaunch = with pkgs; (qutebrowser.overridePythonAttrs (
-  old: {
-    propagatedBuildInputs = (old.propagatedBuildInputs) ++ [
-      (pkgs.python3Packages.buildPythonPackage {
-        name = "qutelaunch";
-        src = fetchFromGitHub {
-          owner = "Kharacternyk";
-          repo = "qutelaunch";
-          rev = "master";
-          sha256 = "1yqjfnqswsjqv26fxlj5hrfnd4cxv0x1bg219q1mykz5gmams8xd";
-        };
-        propagatedBuildInputs = with pkgs; [
-          python3Packages.flask
-        ];
-      })
-    ];
-  }
-)
-); in
-let qb = pkgs.writeShellScriptBin "qb" ''
-  ${qutebrowserWithQutelaunch}/share/qutebrowser/scripts/open_url_in_instance.sh "$@"
-''; in
+let
+  qutebrowserWithQutelaunch = with pkgs; (qutebrowser.overridePythonAttrs (
+    old: {
+      propagatedBuildInputs = old.propagatedBuildInputs ++ [
+        (pkgs.python3Packages.buildPythonPackage {
+          name = "qutelaunch";
+          src = fetchFromGitHub {
+            owner = "Kharacternyk";
+            repo = "qutelaunch";
+            rev = "master";
+            sha256 = "1yqjfnqswsjqv26fxlj5hrfnd4cxv0x1bg219q1mykz5gmams8xd";
+          };
+          propagatedBuildInputs = with pkgs; [
+            python3Packages.flask
+          ];
+        })
+      ];
+    }
+  )
+  );
+  qb = pkgs.writeShellScriptBin "qb" ''
+    ${qutebrowserWithQutelaunch}/share/qutebrowser/scripts/open_url_in_instance.sh "$@"
+  '';
+in
 {
   environment.systemPackages = [
     pkgs.socat
