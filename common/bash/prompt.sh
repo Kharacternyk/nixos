5ca94b4f28b265388db69ead5aa0d9e91c873e36
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2021-11-03 23:16:17 +0200

    Remove the custom prompt for nix-shell.

--- common/bash/prompt.sh
+++ common/bash/prompt.sh
@@ -1,12 +1,10 @@
 BOLD="$(tput bold)"
 RED="$(tput setaf 1)"
 CYAN="$(tput setaf 6)"
-MAGENTA="$(tput setaf 5)"
 RESET="$(tput sgr0)"
 PROMPT_COMMAND='
     ECODE=$?
     PROMPT_COLOR="$BOLD$CYAN"
-    [[ -n $NIX_SHELL ]] && PROMPT_COLOR="$BOLD$MAGENTA"
     [[ $ECODE != 0 ]] && PROMPT_COLOR="$BOLD$RED"
     history -a
     history -n
