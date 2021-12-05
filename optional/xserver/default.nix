38ed5e6667d54f9a8effd034befe9831fa49e281
Nazar Vinnichuk <nazar@vinnich.uk>
2021-12-05 13:25:03 +0200

    Switch to Font Awesome.

--- optional/xserver/default.nix
+++ optional/xserver/default.nix
@@ -76,18 +76,15 @@
           sha256 = "I4i/wr901yu7qp6zglBqsUSRVcetr3I0MBh9KqcuiLc=";
         }
       )
-      (
-        nerdfonts.override {
-          fonts = [ "IBMPlexMono" ];
-        }
-      )
+      font-awesome
       noto-fonts-emoji
       noto-fonts-cjk
     ];
     fontconfig.defaultFonts = {
-      monospace = [ "Blex Mono Nerd Font Mono" ];
+      monospace = [ "IBM Plex Mono" ];
       serif = [ "IBM Plex Serif" ];
       sansSerif = [ "IBM Plex Sans" ];
+      emoji = [ "Noto Emoji" ];
     };
   };
   sound.enable = true;
