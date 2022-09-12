3f87e0ddb770009a5606afb6c27d745910c97db6
Nazar Vinnichuk <nazar@vinnich.uk>
2022-09-12 22:20:07 +0300

    Refactor and fix the virtualisation module.

--- modules/virtualization/default.nix
+++ modules/virtualization/default.nix
@@ -1,6 +1,7 @@
 { dev, ... }: {
-  virtualisation.docker.rootless.enable = dev.docer or false;
-  environment.variables = if !dev.docker or false then { } else {
-    DOCKER_HOST = "unix:///run/user/1000/docker.sock";
-  };
+  virtualisation.docker.rootless.enable = dev.docker or false;
+  environment.variables =
+    if dev.docker or false then {
+      DOCKER_HOST = "unix:///run/user/1000/docker.sock";
+    } else { };
 }
