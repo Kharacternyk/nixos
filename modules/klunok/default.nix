970b847e743ba4546fdb53d065b4e9126948943c
Nazar Vinnichuk <nazar@vinnich.uk>
2023-02-12 18:58:32 +0200

    Update klunok.

--- modules/klunok/default.nix
+++ modules/klunok/default.nix
@@ -11,12 +11,16 @@ let klunok = inputs.klunok.packages.${pkgs.system}.default; in
     path = [
       klunok
       pkgs.acl
+      pkgs.util-linux
     ];
     script = ''
       mkdir -p /klunok
       chown klunok:klunok /klunok
+      for path in /home/nazar /etc/nixos /tmp; do
+        mount --bind $path $path
+      done
       setfacl -m u:klunok:rx -m mask:rx /home/nazar
-      klunok
+      klunok -d /klunok -i /
     '';
   };
   users.users.klunok = {
