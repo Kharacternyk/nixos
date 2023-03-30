0ea9762704411df9f7fe523eb29bd12ea30dce82
Nazar Vinnichuk <nazar@vinnich.uk>
2023-03-30 20:28:12 +0300

    Fix ghost networking interfaces.

--- hosts/nixos-desktop/configuration.nix
+++ hosts/nixos-desktop/configuration.nix
@@ -2,7 +2,6 @@
   imports = [
     ./hardware-configuration.nix
   ];
-  networking.interfaces.wlp2s0u1u4.useDHCP = false;
   services.xserver.displayManager.setupCommands = ''
     ${pkgs.xorg.setxkbmap}/bin/setxkbmap
     ${pkgs.xorg.xset}/bin/xset -dpms
