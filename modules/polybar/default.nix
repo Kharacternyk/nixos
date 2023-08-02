bb46ced2a083bda792b7c5ddcf38654668860920
Nazar Vinnichuk <nazar@vinnich.uk>
2023-08-02 16:54:16 +0300

    Use stock polybar.

--- modules/polybar/default.nix
+++ modules/polybar/default.nix
@@ -1,9 +1,6 @@
 { lib, host, pkgs, ... }: lib.optionalAttrs (host ? hasScreen) {
   environment.systemPackages = [
-    (pkgs.polybar.override {
-      alsaSupport = false;
-      pulseSupport = true;
-    })
+    pkgs.polybar
   ];
   environment.etc."polybar.conf".text = builtins.readFile ./polybar.conf
     + lib.optionalString (!host ? hasGpu) ''
