0fe443c4650ced3f6df0ebe7234fe479c188cef8
Nazar Vinnichuk <nazar@vinnich.uk>
2023-09-25 20:37:44 +0300

    Remove the virtualisation module.

--- modules/virtualisation/default.nix
+++
@@ -1,4 +0,0 @@
-{ lib, host, ... }: lib.optionalAttrs (host ? hasEnoughRam) {
-  virtualisation.docker.rootless.enable = true;
-  environment.variables.DOCKER_HOST = "unix:///run/user/1000/docker.sock";
-}
