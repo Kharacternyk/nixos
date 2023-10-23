3373ab5be1898f03c09de6bf7a51644b2914c606
Nazar Vinnichuk <nazar@vinnich.uk>
2023-10-23 15:32:06 +0300

    Fix a missing bash variable.

--- modules/bash/prompt.sh
+++ modules/bash/prompt.sh
@@ -3,7 +3,7 @@ RED="$(tput setaf 1)"
 CYAN="$(tput setaf 6)"
 RESET="$(tput sgr0)"
 PROMPT_COMMAND='
-    if [[ $ECODE = 0 ]]; then
+    if [[ $? = 0 ]]; then
         PROMPT_COLOR="$BOLD$CYAN"
     else
         PROMPT_COLOR="$BOLD$RED"
