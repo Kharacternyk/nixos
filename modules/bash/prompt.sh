399ebbfbaaf2a812b37213ff64af5fc1feefa4bb
Nazar Vinnichuk <nazar@vinnich.uk>
2023-10-23 15:21:11 +0300

    Refactor the bash prompt command.

--- modules/bash/prompt.sh
+++ modules/bash/prompt.sh
@@ -3,11 +3,12 @@ RED="$(tput setaf 1)"
 CYAN="$(tput setaf 6)"
 RESET="$(tput sgr0)"
 PROMPT_COMMAND='
-    ECODE=$?
-    PROMPT_COLOR="$BOLD$CYAN"
-    [[ $ECODE != 0 ]] && PROMPT_COLOR="$BOLD$RED"
-    history -a
-    history -n
+    if [[ $ECODE = 0 ]]; then
+        PROMPT_COLOR="$BOLD$CYAN"
+    else
+        PROMPT_COLOR="$BOLD$RED"
+    fi
+    history -an
 '
 PS1='\[$PROMPT_COLOR\]\$ \[$RESET\]'
 PS2='\[$PROMPT_COLOR\]> \[$RESET\]'
