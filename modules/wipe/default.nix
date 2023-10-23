9dabe551a298bc9e0b09b3c3c49b0c85ee4dc903
Nazar Vinnichuk <nazar@vinnich.uk>
2023-10-23 16:23:36 +0300

    Improve garbage collection of ephemeral projects.

--- modules/wipe/default.nix
+++ modules/wipe/default.nix
@@ -6,10 +6,31 @@
       ];
       script = ''
         shopt -s nullglob
+        week_ago="$(date -d 'week ago' +%s)"
+
         nix profile wipe-history --profile /nix/var/nix/profiles/system --older-than 3d
-        for profile in /home/*/src/*/.nix/profile; do
-          nix profile wipe-history --profile "$profile"
+
+        for root in /nix/var/nix/gcroots/auto/*; do
+          target="$(readlink "$root")"
+
+          if [[ $? = 0 ]]; then
+            parent="$(dirname "$target")"
+            basename="$(basename "$parent")"
+
+            if [[ $basename = .nix ]]; then
+              gitignore="$parent/.gitignore"
+
+              if [[ -f $gitignore ]]; then
+                last_used="$(date -r "$gitignore" +%s)"
+
+                if (( last_used < week_ago )); then
+                  unlink "$root"
+                fi
+              fi
+            fi
+          fi
         done
+
         nix store gc
       '';
       serviceConfig = {
