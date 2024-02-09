0be79532758e04edec7571bb33ab8fbf0ccdc8b0
Nazar Vinnichuk <nazar@vinnich.uk>
2024-02-09 20:44:13 +0200

    Add a wrapped version of xcolor to sxhkdrc.

--- modules/bspwm/default.nix
+++ modules/bspwm/default.nix
@@ -1,7 +1,16 @@
 { lib, host, pkgs, ... }: lib.optionalAttrs (host ? hasScreen) {
-  environment.systemPackages = [
-    host.inputs.bspwm-utils.defaultPackage.${pkgs.system}
-  ];
+  environment.systemPackages =
+    let
+      xcolor-clutter = pkgs.writeShellScriptBin "xcolor-clutter" ''
+        systemctl --user stop unclutter-xfixes.service
+        xcolor -s
+        systemctl --user start unclutter-xfixes.service
+      '';
+    in
+    [
+      host.inputs.bspwm-utils.defaultPackage.${pkgs.system}
+      xcolor-clutter
+    ];
   services.xserver.windowManager.bspwm =
     let
       bspwmrc = pkgs.writeShellScriptBin "bspwmrc" (builtins.readFile ./bspwmrc);
