09e525dfc56af4d04d05d314bcc7598f35c7c202
Nazar Vinnichuk <nazar@vinnich.uk>
2024-10-09 22:10:09 +0300

    Add a new monitor.

--- hosts/nixos-desktop/configuration.nix
+++ hosts/nixos-desktop/configuration.nix
@@ -23,6 +23,12 @@
         output = "DVI-D-1";
         primary = true;
       }
+      {
+        output = "HDMI-2";
+        monitorConfig = ''
+          Option "RightOf" "DVI-D-1"
+        '';
+      }
     ];
   };
   system.stateVersion = "20.09";
