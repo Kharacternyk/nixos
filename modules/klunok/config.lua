5ccf2d359a2722f9fe62a6c190aabd946847d613
Nazar Vinnichuk <nazar@vinnich.uk>
2024-12-07 20:36:55 +0200

    Track less stuff in klunok.

--- modules/klunok/config.lua
+++ modules/klunok/config.lua
@@ -4,8 +4,10 @@ end
 
 prefix = os.getenv("KLUNOK_PREFIX") or prefix
 
+excluded_paths[home('')] = true
 history_paths[home('.bash_history')] = true
 history_paths[home('.Rhistory')] = true
 included_paths[home('.config/qutebrowser/bookmarks')] = true
 included_paths['/etc/nixos'] = true
+included_paths[home('src')] = true
 project_parents[home('src')] = true
