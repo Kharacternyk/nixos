02dfc8983baa593aae932d6dd84a9a69842d6f2b
Nazar Vinnichuk <nazar@vinnich.uk>
2024-12-07 17:49:59 +0200

    Include /etc/nixos explicitly in klunok backups.

--- modules/klunok/config.lua
+++ modules/klunok/config.lua
@@ -7,4 +7,5 @@ prefix = os.getenv("KLUNOK_PREFIX") or prefix
 history_paths[home('.bash_history')] = true
 history_paths[home('.Rhistory')] = true
 included_paths[home('.config/qutebrowser/bookmarks')] = true
+included_paths['/etc/nixos'] = true
 project_parents[home('src')] = true
