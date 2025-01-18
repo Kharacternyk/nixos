15040b4f24c65614cfdc76fb80d1efeac8c41d13
Nazar Vinnichuk <nazar@vinnich.uk>
2025-01-18 16:47:46 +0200

    Restructure the Klunok configuration

--- modules/klunok/config.lua
+++ modules/klunok/config.lua
@@ -1,14 +1,12 @@
-function home(string)
-    return '/home/nazar/' .. string
-end
-
 prefix = os.getenv("KLUNOK_PREFIX") or prefix
 
-excluded_paths[home('')] = true
-cluded_paths[home('src')] = true
-history_paths[home('.bash_history')] = true
-included_paths[home('.config/qutebrowser/bookmarks')] = true
-included_paths[home('.config/REAPER/reaper.ini')] = true
+excluded_paths['/home/nazar'] = true
+cluded_paths['/home/nazar/src'] = true
+project_parents['/home/nazar/src'] = true
+
+history_paths['/home/nazar/.bash_history'] = true
+included_paths['/home/nazar/.config/qutebrowser/bookmarks'] = true
+included_paths['/home/nazar/.config/REAPER/reaper.ini'] = true
+
 included_paths['/etc/nixos'] = true
-project_parents[home('src')] = true
 project_roots['/etc/nixos'] = true
