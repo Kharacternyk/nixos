f1e187feb858398e149f92a5667802f154215b2c
Nazar Vinnichuk <nazar@vinnich.uk>
2025-02-01 17:18:30 +0200

    Remove a redundant level of nesting.

--- modules/klunok/default.nix
+++ modules/klunok/default.nix
@@ -1,13 +1,11 @@
 { lib, host, pkgs, ... }: {
-  system.activationScripts = {
-    klunok.text = ''
-      mkdir -p /klunok
-      chown klunok:klunok /klunok
-      for path in /home/nazar /home/nazar/.bash_history; do
-        ${pkgs.acl}/bin/setfacl -m u:klunok:rx -m mask:rx $path
-      done
-    '';
-  };
+  system.activationScripts.klunok.text = ''
+    mkdir -p /klunok
+    chown klunok:klunok /klunok
+    for path in /home/nazar /home/nazar/.bash_history; do
+      ${pkgs.acl}/bin/setfacl -m u:klunok:rx -m mask:rx $path
+    done
+  '';
   systemd.services =
     let
       script = ''
