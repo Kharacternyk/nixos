23cdebb2b6a45e5d853227ec4bd46ea99ad8022b
Nazar Vinnichuk <nazar@vinnich.uk>
2025-02-04 22:12:41 +0200

    Avoid tracking a subset of ~/.config with klunok.

--- modules/klunok/config.lua
+++ modules/klunok/config.lua
@@ -5,8 +5,6 @@ cluded_paths['/home/nazar/src'] = true
 project_parents['/home/nazar/src'] = true
 
 history_paths['/home/nazar/.bash_history'] = true
-included_paths['/home/nazar/.config/qutebrowser/bookmarks'] = true
-included_paths['/home/nazar/.config/REAPER/reaper.ini'] = true
 
 included_paths['/etc/nixos'] = true
 project_roots['/etc/nixos'] = true
