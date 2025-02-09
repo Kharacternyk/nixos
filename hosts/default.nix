e48f32185cec497565968b484d191ba2ba770b22
Nazar Vinnichuk <nazar@vinnich.uk>
2025-02-09 22:39:11 +0200

    Rename configuration.nix to default.nix.

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,7 +1,7 @@
 host: host.inputs.nixpkgs.lib.nixosSystem {
   inherit (host) system;
   modules = import ../modules ++ [
-    ./${host.name}/configuration.nix
+    ./${host.name}
   ];
   specialArgs = {
     inherit host;
