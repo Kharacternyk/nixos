7c2c25894b39e73ed54e3ce422649c0a473e3d1c
Nazar Vinnichuk <nazar@vinnich.uk>
2025-04-05 15:30:00 +0300

    Enable containers with GPU access on the server.

--- hosts/nixos-server/default.nix
+++ hosts/nixos-server/default.nix
@@ -2,7 +2,10 @@
   imports = [
     "${modulesPath}/virtualisation/amazon-image.nix"
   ];
-  hardware.graphics.enable = true;
+  hardware = {
+    graphics.enable = true;
+    nvidia-container-toolkit.enable = true;
+  };
   services = {
     amazon-ssm-agent.enable = lib.mkForce false;
     xserver.videoDrivers = [
@@ -10,4 +13,8 @@
     ];
   };
   system.stateVersion = "22.05";
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+  };
 }
