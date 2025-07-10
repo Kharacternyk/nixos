368b971daddd9ac1c3e0d9dd4e7e3414df1e4dbe
Nazar Vinnichuk <nazar@vinnich.uk>
2025-07-10 22:55:50 +0300

    Set up uv.

--- modules/python/default.nix
+++ modules/python/default.nix
@@ -1,19 +1,24 @@
-{ lib, functions, host, pkgs, ... }: {
-  environment.systemPackages = [
+{ lib, functions, host, pkgs, ... }:
+let
+  python = pkgs.python3.withPackages (packages: (
+    functions.readAttributes lib ./packages.txt packages
+  ) ++ (lib.optionals (host ? hasCuda) [
+    packages.torchWithCuda
     (
-      let
-        packages = packages: (
-          functions.readAttributes lib ./packages.txt packages
-        ) ++  (lib.optionals (host ? hasCuda) [
-          packages.torchWithCuda
-          (
-            packages.torchvision.override {
-              torch = packages.torchWithCuda;
-            }
-          )
-        ]);
-      in
-      pkgs.python3.withPackages packages
+      packages.torchvision.override {
+        torch = packages.torchWithCuda;
+      }
     )
-  ];
+  ]));
+in
+{
+  environment = {
+    systemPackages = [
+      python
+    ];
+    variables = {
+      UV_PYTHON = python;
+      UV_PYTHON_PREFERENCE = "only-system";
+    };
+  };
 }
