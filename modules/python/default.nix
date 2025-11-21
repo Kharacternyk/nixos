8fc135b07434055518ab89a17d3f4db1ce0046a7
Nazar Vinnichuk <nazar@vinnich.uk>
2025-11-21 22:46:43 +0200

    Install ale-py.

--- modules/python/default.nix
+++ modules/python/default.nix
@@ -2,29 +2,38 @@
 let
   python = pkgs.python3.withPackages (packages: (
     functions.readAttributes lib ./packages.txt packages
-  ) ++ (lib.optionals (host ? hasCuda) [
-    (
-      packages.gymnasium.overridePythonAttrs {
-        doCheck = false;
-      }
-    )
-    packages.torchWithCuda
-    (
-      packages.einops.override {
-        torch = packages.torchWithCuda;
-      }
-    )
-    (
-      packages.torchmetrics.override {
-        torch = packages.torchWithCuda;
-      }
-    )
-    (
-      packages.torchvision.override {
-        torch = packages.torchWithCuda;
-      }
-    )
-  ]));
+  ) ++ (lib.optionals (host ? hasCuda) (
+    let
+      gymnasium =
+        packages.gymnasium.overridePythonAttrs {
+          doCheck = false;
+        };
+    in
+    [
+      gymnasium
+      (
+        packages.ale-py.override {
+          inherit gymnasium;
+        }
+      )
+      packages.torchWithCuda
+      (
+        packages.einops.override {
+          torch = packages.torchWithCuda;
+        }
+      )
+      (
+        packages.torchmetrics.override {
+          torch = packages.torchWithCuda;
+        }
+      )
+      (
+        packages.torchvision.override {
+          torch = packages.torchWithCuda;
+        }
+      )
+    ]
+  )));
 in
 {
   environment = {
