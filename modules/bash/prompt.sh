45d477beee5e404fe0de12339ccb912cddf9f11a
Nazar Vinnichuk <nazar@vinnich.uk>
2026-03-22 20:50:26 +0200

    Highlight the prompt within lf shells.

--- modules/bash/prompt.sh
+++ modules/bash/prompt.sh
@@ -1,17 +1,25 @@
 BOLD="$(tput bold)"
 RED="$(tput setaf 1)"
 GREEN="$(tput setaf 2)"
+MAGENTA="$(tput setaf 5)"
 CYAN="$(tput setaf 6)"
 RESET="$(tput sgr0)"
 PROMPT_COMMAND='
     if [[ $? = 0 ]]; then
-        case $HOSTNAME in
-            *server*) PROMPT_COLOR="$BOLD$GREEN" ;;
-            *) PROMPT_COLOR="$BOLD$CYAN" ;;
-        esac
+        if [[ -z $f ]]; then
+            case $HOSTNAME in
+                *server*) PROMPT_COLOR="$GREEN" ;;
+                *) PROMPT_COLOR="$CYAN" ;;
+            esac
+        else
+            PROMPT_COLOR="$MAGENTA"
+        fi
     else
-        PROMPT_COLOR="$BOLD$RED"
+        PROMPT_COLOR="$RED"
     fi
+
+    PROMPT_COLOR="$BOLD$PROMPT_COLOR"
+    
     history -a
     history -n
 '
