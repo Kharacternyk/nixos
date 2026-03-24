68fe2b93ce08eeb63834ac8bdb30f8c60df7f715
Nazar Vinnichuk <nazar@vinnich.uk>
2026-03-24 14:54:54 +0200

    Update Klunok.

--- modules/klunok/config.lua
+++ modules/klunok/config.lua
@@ -3,6 +3,8 @@ suffix = os.getenv("KLUNOK_SUFFIX") or ""
 
 journal_timestamp_pattern = journal_timestamp_pattern .. suffix
 
+working_copy_link_name = 'wip'
+
 excluded_paths['/home/nazar'] = true
 cluded_paths['/home/nazar/src'] = true
 project_parents['/home/nazar/src'] = true
