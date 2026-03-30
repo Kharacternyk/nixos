d9ee012c32aaa3cefe8b127a96df56e57aa7ab42
Nazar Vinnichuk <nazar@vinnich.uk>
2026-03-30 15:27:17 +0300

    Power off the server automatically.

--- hosts/nixos-server/default.nix
+++ hosts/nixos-server/default.nix
@@ -1,4 +1,4 @@
-{ lib, modulesPath, ... }: {
+{ lib, modulesPath, pkgs, ... }: {
   imports = [
     "${modulesPath}/virtualisation/amazon-image.nix"
   ];
@@ -14,4 +14,44 @@
     ];
   };
   system.stateVersion = "22.05";
+  systemd = {
+    services.auto-poweroff = {
+      path = [
+        pkgs.procps
+        pkgs.systemd
+      ];
+      script = ''
+        set -eu
+
+        [[ -n $(loginctl list-sessions --no-legend) ]] && exit
+        pgrep tmux >/dev/null && exit
+
+        logout_line="$(journalctl --unit=systemd-logind --grep='Removed session' -n 1 -o short-unix || true)"
+
+        [[ -z "$logout_line" ]] && exit
+
+        now="$(date +%s)"
+        last_logout="''${logout_line%% *}"
+        last_logout="''${last_logout%%.*}"
+
+        [[ $((now - last_logout)) -lt $((30 * 60)) ]] && exit
+
+        systemctl poweroff
+      '';
+      serviceConfig = {
+        Restart = "no";
+        Type = "oneshot";
+      };
+    };
+    timers.auto-poweroff = {
+      timerConfig = {
+        OnBootSec = "45m";
+        OnUnitActiveSec = "5m";
+        Unit = "auto-poweroff.service";
+      };
+      wantedBy = [
+        "timers.target"
+      ];
+    };
+  };
 }
