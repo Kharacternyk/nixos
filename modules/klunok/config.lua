55925db29efd2efbf1c9dc5997f87e2dbcd0180f
Nazar Vinnichuk <nazar@vinnich.uk>
2026-04-05 20:50:27 +0300

    Set up StyLua.

--- modules/klunok/config.lua
+++ modules/klunok/config.lua
@@ -3,13 +3,13 @@ suffix = os.getenv("KLUNOK_SUFFIX") or ""
 
 journal_timestamp_pattern = journal_timestamp_pattern .. suffix
 
-working_copy_link_name = 'wip'
+working_copy_link_name = "wip"
 
-excluded_paths['/home/nazar'] = true
-cluded_paths['/home/nazar/src'] = true
-project_parents['/home/nazar/src'] = true
+excluded_paths["/home/nazar"] = true
+cluded_paths["/home/nazar/src"] = true
+project_parents["/home/nazar/src"] = true
 
-history_paths['/home/nazar/.bash_history'] = true
+history_paths["/home/nazar/.bash_history"] = true
 
 editors["uutils-coreutils"] = true
-project_roots['/etc/nixos'] = true
+project_roots["/etc/nixos"] = true
--- modules/neovim/init.lua
+++ modules/neovim/init.lua
@@ -1,101 +1,103 @@
 vim.diagnostic.config({
-    virtual_text = true,
+  virtual_text = true,
 })
 
 vim.lsp.enable({
-    "clangd",
-    "ty",
-    "nixd",
+  "clangd",
+  "ty",
+  "nixd",
 })
 
 require("conform").setup({
-    format_on_save = {},
-    formatters_by_ft = {
-        c = {
-            "clang-format",
-        },
-        python = {
-            "ruff_format",
-            "ruff_fix",
-            "ruff_organize_imports",
-        },
-        nix = {
-            "nixpkgs_fmt",
-        },
+  format_on_save = {},
+  formatters_by_ft = {
+    c = {
+      "clang-format",
     },
+    lua = {
+      "stylua",
+    },
+    nix = {
+      "nixpkgs_fmt",
+    },
+    python = {
+      "ruff_format",
+      "ruff_fix",
+      "ruff_organize_imports",
+    },
+  },
 })
 
 require("blink.cmp").setup({
-    completion = {
-        documentation = {
-            auto_show = true,
+  completion = {
+    documentation = {
+      auto_show = true,
+    },
+    list = {
+      selection = {
+        preselect = false,
+      },
+    },
+    menu = {
+      draw = {
+        columns = {
+          {
+            "label",
+            "kind",
+            gap = 2,
+          },
+          {
+            "source_name",
+          },
         },
-        list = {
-            selection = {
-                preselect = false,
+        components = {
+          label = {
+            width = {
+              max = 48,
             },
+          },
         },
-        menu = {
-            draw = {
-                columns = {
-                    {
-                        "label",
-                        "kind",
-                        gap = 2,
-                    },
-                    {
-                        "source_name",
-                    },
-                },
-                components = {
-                    label = {
-                        width = {
-                            max = 48,
-                        },
-                    },
-                },
-                treesitter = {
-                    "lsp",
-                },
-            },
-            max_height = 11,
-            scrolloff = 100,
+        treesitter = {
+          "lsp",
         },
+      },
+      max_height = 11,
+      scrolloff = 100,
     },
-    keymap = {
-        preset = "none",
-        ["<C-h>"] = {
-            "cancel",
-            "fallback",
-        },
-        ["<C-k>"] = {
-            "show_signature",
-            "hide_signature",
-            "fallback",
-        },
-        ["<C-l>"] = {
-            "show",
-            "select_and_accept",
-            "fallback",
-        },
-        ["<C-n>"] = {
-            "scroll_documentation_down",
-        },
-        ["<C-p>"] = {
-            "scroll_documentation_up",
-        },
-        ["<Tab>"] = {
-            "select_next",
-            "snippet_forward",
-            "fallback",
-        },
-        ["<S-Tab>"] = {
-            "select_prev",
-            "snippet_backward",
-            "fallback",
-        },
+  },
+  keymap = {
+    preset = "none",
+    ["<C-h>"] = {
+      "cancel",
+      "fallback",
     },
-    signature = {
-        enabled = true,
+    ["<C-k>"] = {
+      "show_signature",
+      "hide_signature",
+      "fallback",
     },
-})
+    ["<C-l>"] = {
+      "show",
+      "select_and_accept",
+      "fallback",
+    },
+    ["<C-n>"] = {
+      "scroll_documentation_down",
+    },
+    ["<C-p>"] = {
+      "scroll_documentation_up",
+    },
+    ["<Tab>"] = {
+      "select_next",
+      "snippet_forward",
+      "fallback",
+    },
+    ["<S-Tab>"] = {
+      "select_prev",
+      "snippet_backward",
+      "fallback",
+    },
+  },
+  signature = {
+    enabled = true,
+  },
